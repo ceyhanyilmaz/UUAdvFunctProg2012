5cb49b2f2e1923a3cd0a8e221bbe473288d2bfe8
Anders Hassis <anders.hassis@gmail.com>
2012-11-19 13:55:31 +0100

    Part 2 on assigment 1 done

--- assignment1.erl
+++ assignment1.erl
@@ -4,22 +4,27 @@
 %
 -module(assignment1).
 
--export([dividers_of/1]).
-%-export([dividers_of/1, primes_up_to/1]).
+-export([dividers_of/1, primes_up_to/1]).
 %-export([fibonacci_tree/1]).
 %-export([factorize_initial_state/0, factorize/2, factorize_dispose_state/1]).
 
+% Part 1: a - dividers_of
 -spec dividers_of(integer()) -> [integer()].
+dividers_aux(N, N) -> [];
+dividers_aux(I, N) when N rem I == 0 -> [I | dividers_aux(I+1, N)];
+dividers_aux(I, N) -> dividers_aux(I+1, N).
 
-dividers_aux(N,N) -> [];
-dividers_aux(I,N) when N rem I == 0 -> [I | dividers_aux(I+1,N)];
-dividers_aux(I,N) -> dividers_aux(I+1,N).
-
-dividers_of(N) -> dividers_aux(2,N).
-
+dividers_of(N) -> dividers_aux(2, N).
 %assignment1:dividers_of(20).% == [2,4,5,10].
 
-%-spec primes_up_to(integer()) -> [integer()].
+% Part 1: b - primes_up_to
+-spec primes_up_to(integer()) -> [integer()].
+primes_up_to([]) -> [];
+primes_up_to([H|T]) -> 
+	List = lists:filter(fun(N) -> N rem H /= 0 end, T),
+	[H|primes_up_to(List)];
+primes_up_to(N) -> primes_up_to(lists:seq(2,N)).
+
 %primes_up_to(10) -> [2,3,5,7].
 
 %fibonacci_tree(3) -> 5;

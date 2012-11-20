45a213fb5fe069cfa53756d12352e18e8ec1cbbc
Konve <jonatan.jansson@gmail.com>
2012-11-20 14:36:59 +0100

    rewrote task 1 using list comprehension

--- assignment1.erl
+++ assignment1.erl
@@ -12,22 +12,13 @@
 
 % Part 1: a - dividers_of
 -spec dividers_of(integer()) -> [integer()].
-dividers_aux(N, N) -> [];
-dividers_aux(I, N) when N rem I == 0 -> [I | dividers_aux(I+1, N)];
-dividers_aux(I, N) -> dividers_aux(I+1, N).
+dividers_of(N) -> [X || X <- lists:seq(2,N-1), N rem X == 0].
 
-dividers_of(N) -> dividers_aux(2, N).
 %assignment1:dividers_of(20).% == [2,4,5,10].
 
 % Part 1: b - primes_up_to
 -spec primes_up_to(integer()) -> [integer()].
-primes_up_to([]) -> [];
-primes_up_to([H|T]) -> 
-	List = lists:filter(fun(N) -> N rem H /= 0 end, T),
-	[H|primes_up_to(List)];
-primes_up_to(N) -> primes_up_to(lists:seq(2,N)).
-
-%primes_up_to(10) -> [2,3,5,7].
+primes_up_to(N) -> [X || X <- lists:seq(2,N), dividers_of(X) ==  []].
 
 
 % Part 2: fibonacci_tree
@@ -36,7 +27,6 @@ primes_up_to(N) -> primes_up_to(lists:seq(2,N)).
 fibonacci_tree_aux(N) when N =< 1 -> 
 	receive 
 		{From, sum} ->
-			%io:format("Received message ~p~n", [sum]),
 			From ! {self(), 1}
 	end;
 fibonacci_tree_aux(N) -> 
@@ -50,12 +40,10 @@ fibonacci_tree_aux(N) ->
 
 			receive 
 				{PIDleft, SumLeft} -> ok
-                    %io:format("Received message from left ~p~n", [SumLeft])
 			end,
 
 			receive 
 				{PIDright, SumRight} -> ok
-                    %io:format("Received message from right ~p~n", [SumRight])
 			end,
 			From ! {self(), SumLeft+SumRight+1}
 	end.
@@ -71,12 +59,10 @@ fibonacci_tree(N) ->
 
 	receive 
 		{PIDleft, SumLeft} -> ok
-            %io:format("Received message from left ~p~n", [SumLeft])
 	end,
 
 	receive 
 		{PIDright, SumRight} -> ok
-            %io:format("Received message from right ~p~n", [SumRight])
 	end,
 	SumLeft+SumRight+1.
 

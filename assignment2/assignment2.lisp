483498f8808046c8bf42d0e1f32148e274fa14f8
Anders Hassis <anders.hassis@gmail.com>
2012-12-03 15:13:40 +0100

    Part2 not done

--- assignment2/assignment2.lisp
+++ assignment2/assignment2.lisp
@@ -1,21 +1,39 @@
-
 ; Structs
+(defstruct treedict root compare)
 (defstruct treenode key value leftchild rightchild)
 
 ; Functions
-;(make-treedict :nodes (make-treenode) :compare compare)
-
-(defun make-treedict (parameter*)
-
-	) 
+(defun stringcompare (str1 str2)
+	; TODO: Fixa för case
+	(if (string= str1 str2) 
+		EQ
+	  (if (string< str1 str2) 
+		  LT
+		GT)))
 
 (defun create-dictionary (&key compare)
+	(if compare
+		(make-treedict :root nil :compare compare)
+	  (make-treedict :root nil :compare #'stringcompare)))
 
-	) 
+(defun updatenode (key value node 'compare)
+	(if (null node)
+	  	(make-treenode :key key :value value :leftchild nil :rightchild nil)
+	  (case (compare key node-key)
+	  	(LT (make-treenode :key node-key :value node-value
+	  	 :leftchild (updatenode key value leftchild 'compare) 
+	  	 :rightchild node-rightchild))
+	  	(GT (make-treenode :key node-key :value node-value
+	  	 :leftchild node-lefchild
+	  	 :rightchild (updatenode key value rightchild 'compare)))
+	  	(EQ (make-treenode :key key :value value
+	  		:leftchild leftchild :rightchild rightchild)))))
+
+(defun update (key value dict)
+	(make-treedict :root (updatenode key value 'dict-root 'dict-compare)
+		:compare 'dict-compare))
 
 (defun lookup (key dict &key default)) 
-
-(defun update (key value dict)) 
 
 (defun fold (dict initial)) 
 
